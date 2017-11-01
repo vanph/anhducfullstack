891e2f9a18da4897ced8866f0cbf17c015f84e71
blackeye68 <tu.cuong.bat.tuc@gmail.com>
2017-11-01 23:48:51 +0700

    Update Lesson1

--- source/dev/Tutorial/Lesson1/Program.cs
+++ source/dev/Tutorial/Lesson1/Program.cs
@@ -11,7 +11,7 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            var message = "Hello world";
+            var message = "Hello world 2222";
 
             Console.WriteLine(message);
 
