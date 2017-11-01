75390f6d8706e79c7e8df14ec867f76af149ecc2
blackeye68 <tu.cuong.bat.tuc@gmail.com>
2017-11-01 23:47:01 +0700

    Add Lesson1

---
+++ source/dev/Tutorial/Lesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "Hello world";
+
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
