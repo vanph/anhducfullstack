1bcc49ed8bf74ac220e5b42f6ce3b312dbf32567
tu.cuong.bat.tuc@gmail.com <24867193@vohoi>
2018-01-07 23:55:57 +0700

    update code sum function given number

--- source/dev/Tutorial/ValueTypes/Program.cs
+++ source/dev/Tutorial/ValueTypes/Program.cs
@@ -17,11 +17,24 @@ namespace ValueTypes
             //Console.WriteLine($"max of decimal:{decimal.MaxValue}");
             //Console.WriteLine($"max of double:{double.MaxValue}");
             Console.WriteLine("Please input a number:");
-            
-            var sum = Sum(1000);
+            //get number
+            string numberText = Console.ReadLine();
+            try
+            {
+                //int number = Convert.ToInt32(numberText);
+                int givenNumber = int.Parse(numberText);
+                //int.TryParse
+                int sum = SumEvenNumbers(givenNumber);
+               
 
-            Console.WriteLine(sum);
-
+                Console.WriteLine(sum);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+                       
+            //pause
             Console.ReadLine();           
         }
 
@@ -30,6 +43,36 @@ namespace ValueTypes
             var sum = 0;
 
             for (var i = 1; i <= number; i++)
+            {
+                sum = sum + i;
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Sum of even numbers that less than or equal the given number
+        /// </summary>
+        /// <param name="givenNumber"></param>
+        /// <returns></returns>
+        private static int SumEvenNumbers(int givenNumber)
+        {
+            //For exmaple:
+            //given number = 10
+            //=> retunrn : 2 + 4 + 6 + 8 + 10
+            //given number = 5 => return : 2+ 4
+
+            var sum = 0;
+
+            //for (var i = 1; i <= givenNumber; i++)
+            //{
+            //    if(i%2 == 0)
+            //    {
+            //        sum = sum + i;
+            //    }               
+            //}
+
+            for (var i = 2; i <= givenNumber; i=i+2)
             {
                 sum = sum + i;
             }
