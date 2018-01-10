ae72cebcefd8afb543201a9bf3f2ecd2b39e6cba
tu.cuong.bat.tuc@gmail.com <24867193@vohoi>
2018-01-11 00:15:59 +0700

    do homework one

---
+++ source/dev/Tutorial/PrimeNumberLess1000/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberLess1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool PrimeNumberCorrect = true;
+            Console.WriteLine("PrimeNumber is: ");
+            for (int i = 2; i<1000; i++)
+            {
+                for (int j = 2; j< 1000; j++)
+                {
+                    if (i != j && i % j ==0)
+                    {
+                        PrimeNumberCorrect = false;
+                        break;
+                    }
+                }
+                if (PrimeNumberCorrect)
+                {
+                    Console.Write("\t" + i);
+                }
+                PrimeNumberCorrect = true;
+            }
+            Console.ReadKey();
+        }
+    }
+}
