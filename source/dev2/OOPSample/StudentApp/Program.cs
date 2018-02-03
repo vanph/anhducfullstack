2ec2186126848f35b983b5ecb55dd4275774e851
vanph <vanphbk@gmail.com>
2018-02-03 23:58:11 +0700

    OOP- Student app

---
+++ source/dev2/OOPSample/StudentApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentApp.Models;
+
+namespace StudentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student
+            {
+                FirstName = "Duc",
+                LastName = "Ngo",
+                Age = 20
+            };
+
+            student.Greet();
+            student.GoToClasses();
+            student.ShowAge();
+            
+            var teacher = new Teacher
+            {
+                FirstName = "Park",
+                LastName = "Hang Seo",
+                Age = 50
+            };
+
+            teacher.Greet();
+            teacher.Explain();
+
+            Console.ReadLine();
+        }
+    }
+}
